28aadd1cd7b181938e2699981ca35ded048da173
vietdv277 <vietdv277@gmail.com>
2022-09-08 17:46:08 +0700

    Add new server init script

---
+++ hubgame_server_init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Fix AppStream error
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.epel.cloud|g' /etc/yum.repos.d/CentOS-Linux-*
+
+# Install epel-release and update
+sudo yum -y install epel-release 
+sudo yum -y update
+
+# Install support tools
+sudo yum install -y nano vim htop yum-utils 
+
+# Install and configure git
+sudo yum install -y git
+sudo git config --global pull.rebase false
+
+# Install Java and Maven
+sudo yum install -y java-1.8.0-openjdk maven
+
+# Install docker
+## Remove old version
+sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+## Install docker
+sudo yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+## Enable and start docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Install nginx
+sudo yum install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
