28d997e6629638808c1aaf8210a131ba40d23b6d
vietdv277 <vietdv277@gmail.com>
2022-09-09 12:12:57 +0700

    Migrate to CentOS Stream 8

--- hubgame_server_init.sh
+++ hubgame_server_init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Fix AppStream error
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.epel.cloud|g' /etc/yum.repos.d/CentOS-*
+# sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.epel.cloud|g' /etc/yum.repos.d/CentOS-*
+# Migrate to CentOS Stream 8
+dnf --disablerepo '*' --enablerepo=extras swap centos-linux-repos centos-stream-repos -y
+dnf distro-sync -y
 
 # Install epel-release and update
 sudo yum -y install epel-release 
